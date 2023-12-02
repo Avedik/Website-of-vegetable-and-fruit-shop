1d210bcb90ca0cfe2bdc389cef6010ec65e6c1ac
Avedik <43447356+Avedik@users.noreply.github.com>
2023-12-02 10:53:44 +0000

    Add test content

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,27 @@
 class MainController < ApplicationController
+  def index; end
 
-def index
-end
+  def login; end
+
+  def about; end
+
+  def account; end
+
+  def blog; end
+
+  def cart; end
+
+  def checkout; end
+
+  def compare; end
+
+  def contact; end
+
+  def product; end
+
+  def register; end
+
+  def shop; end
+
+  def wishlist; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
    root "main#index"
+  get "/index.html", to: "main#index"
+  get "/login.html", to: "main#login"
+  get "/about.html", to: "main#about"
+  get "/account.html", to: "main#account"
+  get "/blog.html", to: "main#blog"
+  get "/cart.html", to: "main#cart"
+  get "/checkout.html", to: "main#checkout"
+  get "/compare.html", to: "main#compare"
+  get "/contact.html", to: "main#contact"
+  get "/product.html", to: "main#product"
+  get "/register.html", to: "main#register"
+  get "/shop.html", to: "main#shop"
+  get "/wishlist.html", to: "main#wishlist"
+  get "/404.html", to: "main#index"
+  get "/blog-details.html", to: "main#index"
+  get "/forgot-password.html", to: "main#index"
+  get "/index-2.html", to: "main#index"
+  get "/index-3.html", to: "main#index"
 end
