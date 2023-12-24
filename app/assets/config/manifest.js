284f749b9d208b77c8db8f0a370524bc0e530e1f
Avedik <43447356+Avedik@users.noreply.github.com>
2023-12-24 06:10:21 +0000

    Add links in manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,17 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../javascripts .js
+//= link meanmenu.min.css
+//= link_directory ../fonts .woff
+
+//= link animate.css
+//= link bootstrap.min.css
+//= link nivo-slider.css
+//= link owl.carousel.min.css
+//= link font-awesome.min.css
+//= link slick.css
+//= link style.css
+//= link jquery-ui.min.css
+//= link default.css
+//= link responsive.css
+//= link jquery.fancybox.css
